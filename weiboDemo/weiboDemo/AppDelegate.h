c9e501c64d9dd1b0765c9ef57607c7ecbc1dd3ed
fanhoucheng <1185611693@qq.com>
2016-09-09 16:58:33 +0800

    项目初始化

---
+++ weiboDemo/weiboDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weiboDemo
+//
+//  Created by fanhoucheng on 16/9/9.
+//  Copyright © 2016年 fanhoucheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weiboDemo/weiboDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weiboDemo
+//
+//  Created by fanhoucheng on 16/9/9.
+//  Copyright © 2016年 fanhoucheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
